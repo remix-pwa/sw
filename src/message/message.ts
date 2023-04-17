08d905b988a1889617df5f4ed276e547b7fbdd38
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-17 14:51:46 +0100

    chore: main message superclass :+1:

---
+++ src/message/message.ts
@@ -0,0 +1,29 @@
+import { MessagePlugin } from "../plugins/interfaces/messagePlugin.js";
+import { MessageEnv } from "./types.js";
+
+export abstract class MessageHandler {
+  plugins: MessagePlugin[];
+  state: MessageEnv;
+
+  constructor(plugins: MessagePlugin[] = [], state: MessageEnv = {}) {
+    this.plugins = plugins;
+    this.state = state;
+  }
+
+  async handle(event: ExtendableMessageEvent, state: Record<string, any> = {}) {
+    await this._handleMessage(event, state);
+  }
+
+  abstract _handleMessage(
+    event: ExtendableMessageEvent,
+    state: Record<string, any>
+  ): Promise<void> | void;
+
+  async runPlugins(hook: keyof MessagePlugin, env: MessageEnv) {
+    for (const plugin of this.plugins) {
+      if (plugin[hook]) {
+        await plugin[hook]!(env);
+      }
+    }
+  }
+}
