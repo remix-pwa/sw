c1f31ef77f5c13d77fe3b148b567a62498905f28
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-17 14:52:11 +0100

    message plugin interface

---
+++ src/plugins/interfaces/messagePlugin.ts
@@ -0,0 +1,6 @@
+import { MessageEnv } from "../../message/types.js";
+
+export interface MessagePlugin {
+  messageDidReceive?: (env: MessageEnv) => void;
+  messageWillSend?: (env: MessageEnv) => void;
+}
