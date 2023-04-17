e6fb5ce16f0b01528b36181ebeaf94b7a5f66a6e
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-17 14:43:42 +0100

    chore: helper functions

---
+++ src/core/helper.ts
@@ -0,0 +1,19 @@
+/**
+ * Helper functions used across the codebase. 
+ */
+
+export const isHttpRequest = (request: Request): boolean => {
+  return request.url.startsWith("http");
+};
+
+export function isDev(): boolean {
+  return process.env.NODE_ENV === "development";
+}
+
+export function toError(error: unknown): Error {
+  if (error instanceof Error) {
+    return error;
+  }
+
+  return error as Error;
+}
