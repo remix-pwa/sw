cbdeecd52038aa7ebefa8701fafd02c9bee9c1dc
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-17 15:06:24 +0100

    chore: Remix + Workbox plugin

---
+++ src/workbox/loaderPlugin.ts
@@ -0,0 +1,41 @@
+import type {
+  CachedResponseWillBeUsedCallback,
+  HandlerDidErrorCallback,
+  CachedResponseWillBeUsedCallbackParam,
+  FetchDidSucceedCallback,
+  FetchDidSucceedCallbackParam,
+} from "workbox-core/types.js";
+
+/* Plugins */
+
+type RemixLoaderPlugin = {
+  cachedResponseWillBeUsed: CachedResponseWillBeUsedCallback;
+  handlerDidError: HandlerDidErrorCallback;
+  fetchDidSucceed: FetchDidSucceedCallback;
+};
+
+// Loader Plugin
+export const remixLoaderPlugin: RemixLoaderPlugin = {
+  fetchDidSucceed: async ({ response }: FetchDidSucceedCallbackParam) => {
+    // @ts-ignore
+    console.log("manifest", self.__remixManifest);
+    return response;
+  },
+  cachedResponseWillBeUsed: async ({
+    cachedResponse,
+  }: CachedResponseWillBeUsedCallbackParam) => {
+    cachedResponse?.headers.set("X-Remix-Worker", "yes");
+    return cachedResponse;
+  },
+  handlerDidError: async () => {
+    return new Response(JSON.stringify({ message: "Network Error" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "X-Remix-Catch": "yes",
+        "X-Remix-Worker": "yes",
+      },
+    });
+  },
+};
