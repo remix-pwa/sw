82ee72177e44a2d146f711e98de9c4d7e6e75764
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-17 14:44:10 +0100

    typings for `strategy` sub-module

---
+++ src/strategy/types.ts
@@ -0,0 +1,24 @@
+/**
+ * Contains global types for this sub-module
+ */
+
+import { StrategyPlugin } from '../plugins/strategyPlugin.js';
+
+export interface CacheQueryMatchOptions
+  extends Omit<CacheQueryOptions, 'cacheName' | 'ignoreMethod'> {}
+
+export interface CacheStrategyOptions {
+  cacheName?: string;
+  plugins?: StrategyPlugin[];
+  isLoader?: boolean;
+  matchOptions?: CacheQueryMatchOptions;
+}
+
+export interface FetchListenerEnvState extends Record<string, any> {
+  fetcher?: typeof fetch;
+}
+
+export interface FetchListenerEnv {
+  event?: FetchEvent;
+  state?: FetchListenerEnvState;
+}
