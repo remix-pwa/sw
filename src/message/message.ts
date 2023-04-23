ff331d3fc33fadeb68136ddb7f7b96ba185af029
Abdur-Rahman <abbadshaf05@gmail.com>
2023-04-23 14:38:06 +0100

    chore: Updated message handler

--- src/message/message.ts
+++ src/message/message.ts
@@ -1,20 +1,32 @@
 import { MessagePlugin } from '../plugins/interfaces/messagePlugin.js';
 import { MessageEnv } from './types.js';
 
+export type MessageHandlerParams = {
+  plugins?: MessagePlugin[];
+  state?: MessageEnv;
+}
+
 export abstract class MessageHandler {
+  /**
+   * The plugins array is used to run plugins before and after the message handler.
+   * They are passed in when the handler is initialised.
+   */
   plugins: MessagePlugin[];
+  /**
+   * The state object is used to pass data between plugins.
+   */
   state: MessageEnv;
 
-  constructor(plugins: MessagePlugin[] = [], state: MessageEnv = {}) {
-    this.plugins = plugins;
-    this.state = state;
+  constructor({plugins, state}: MessageHandlerParams) {
+    this.plugins = plugins || [];
+    this.state = state || {};
   }
 
   async handle(event: ExtendableMessageEvent, state: Record<string, any> = {}) {
     await this._handleMessage(event, state);
   }
 
-  abstract _handleMessage(
+  protected abstract _handleMessage(
     event: ExtendableMessageEvent,
     state: Record<string, any>
   ): Promise<void> | void;
