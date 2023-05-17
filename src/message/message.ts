a16af5456d7f81b7bb3ac8ace4f1ce58a01fec21
ShafSpecs <abbadshaf05@gmail.com>
2023-05-17 17:21:13 +0100

    added extra checks and scoping to fields

--- src/message/message.ts
+++ src/message/message.ts
@@ -11,11 +11,12 @@ export abstract class MessageHandler {
    * The plugins array is used to run plugins before and after the message handler.
    * They are passed in when the handler is initialised.
    */
-  plugins: MessagePlugin[];
+  protected plugins: MessagePlugin[];
+
   /**
    * The state object is used to pass data between plugins.
    */
-  state: MessageEnv;
+  protected state: MessageEnv;
 
   constructor({ plugins, state }: MessageHandlerParams = {}) {
     this.plugins = plugins || [];
@@ -37,7 +38,7 @@ export abstract class MessageHandler {
     state: Record<string, any>
   ): Promise<void> | void;
 
-  async runPlugins(hook: keyof MessagePlugin, env: MessageEnv) {
+  protected async runPlugins(hook: keyof MessagePlugin, env: MessageEnv) {
     for (const plugin of this.plugins) {
       if (plugin[hook]) {
         await plugin[hook]!(env);
