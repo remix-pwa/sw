fb088b60ea44566023f96ba8f4f8d12be88fd757
ShafSpecs <abbadshaf05@gmail.com>
2023-05-24 23:49:57 +0100

    fix: Implemented plugin lifecycles

--- src/strategy/networkFirst.ts
+++ src/strategy/networkFirst.ts
@@ -14,8 +14,8 @@ export class NetworkFirst extends CacheStrategy {
     super(options);
 
     this.fetchListenerEnv = env;
-    // Default timeout of 10 seconds
-    this._networkTimeoutSeconds = options.networkTimeoutSeconds || 10;
+    // Default timeout of `Infinity`
+    this._networkTimeoutSeconds = options.networkTimeoutSeconds || Infinity;
   }
 
   override async _handle(request: Request) {
@@ -24,19 +24,9 @@ export class NetworkFirst extends CacheStrategy {
     try {
       const response = await this.fetchAndCache(request);
 
-      for (const plugin of this.plugins) {
-        if (plugin.fetchDidSucceed)
-          await plugin.fetchDidSucceed({ request, response });
-      }
-
       return response;
     } catch (error) {
       let err = toError(error);
-
-      for (const plugin of this.plugins) {
-        if (plugin.fetchDidFail)
-          await plugin.fetchDidFail({ request, error: err });
-      }
 
       const cachedResponse = await cache.match(request, this.matchOptions);
 
@@ -56,28 +46,97 @@ export class NetworkFirst extends CacheStrategy {
   private async fetchAndCache(request: Request): Promise<Response> {
     const cache = await caches.open(this.cacheName);
 
-    const timeoutPromise = this._networkTimeoutSeconds
-      ? new Promise<Response>((_, reject) => {
-          setTimeout(() => {
-            reject(
-              new Error(
-                `Network timed out after ${this._networkTimeoutSeconds} seconds`
-              )
-            );
-          }, this._networkTimeoutSeconds * 1000);
-        })
-      : null;
+    const timeoutPromise =
+      this._networkTimeoutSeconds !== Infinity
+        ? new Promise<Response>((_, reject) => {
+            setTimeout(() => {
+              reject(
+                new Error(
+                  `Network timed out after ${this._networkTimeoutSeconds} seconds`
+                )
+              );
+            }, this._networkTimeoutSeconds * 1000);
+          })
+        : null;
 
     const fetcher = this.fetchListenerEnv.state?.fetcher || fetch;
 
-    const fetchPromise = fetcher(request);
+    let updatedRequest = request;
 
-    const response = timeoutPromise
+    for (const plugin of this.plugins) {
+      if (plugin.requestWillFetch) {
+        updatedRequest = await plugin.requestWillFetch({
+          request: updatedRequest
+        });
+      }
+    }
+
+    const fetchPromise = fetcher(updatedRequest).catch((err) => {
+      for (const plugin of this.plugins) {
+        if (plugin.fetchDidFail)
+          plugin.fetchDidFail({
+            request: updatedRequest,
+            error: err as unknown as Error
+          });
+      }
+    });
+
+    let response = timeoutPromise
       ? await Promise.race([fetchPromise, timeoutPromise])
       : await fetchPromise;
 
-    await cache.put(request, response.clone());
+    // If the fetch was successful, then proceed along else throw an error 
+    if (response) {
+      // `fetchDidSucceed` performs some changes to response so store it elsewhere 
+      // to avoid overtyping original variable
+      let updatedResponse: Response = response;
 
-    return response;
+      for (const plugin of this.plugins) {
+        if (plugin.fetchDidSucceed) {
+          updatedResponse = await plugin.fetchDidSucceed({
+            request: updatedRequest,
+            response: updatedResponse
+          });
+        }
+      }
+
+      // `null` can be returned here to avoid caching resourced. Hence store in 
+      // a new variable that can be checked for if null. 
+      let aboutToBeCachedResponse: Response | null = updatedResponse;
+
+      for (const plugin of this.plugins) {
+        if (plugin.cacheWillUpdate) {
+          aboutToBeCachedResponse = await plugin.cacheWillUpdate({
+            request: updatedRequest,
+            response: aboutToBeCachedResponse!
+          });
+
+          if (!aboutToBeCachedResponse) {
+            break;
+          }
+        }
+      }
+
+      // If response wasn't null, update cache and return the response
+      if (aboutToBeCachedResponse) {
+        await cache.put(request, response.clone());
+
+        for (const plugin of this.plugins) {
+          if (plugin.cacheDidUpdate) {
+            await plugin.cacheDidUpdate({
+              request: updatedRequest,
+              cacheName: this.cacheName,
+              newResponse: updatedResponse
+            });
+          }
+        }
+
+        return aboutToBeCachedResponse;
+      }
+
+      return updatedResponse;
+    }
+
+    throw new Error('No response received from fetch: Timeout');
   }
 }
