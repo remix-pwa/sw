1603223b4becf32ac5f85783dcc0e078fc499ef2
ShafSpecs <abbadshaf05@gmail.com>
2023-05-24 21:57:30 +0100

    chore: standardised `StrategyPlugin`

--- src/plugins/interfaces/strategyPlugin.ts
+++ src/plugins/interfaces/strategyPlugin.ts
@@ -6,28 +6,32 @@ import { CacheQueryMatchOptions } from '../../strategy/types.js';
 export declare interface StrategyPlugin {
   // Called before a request is made to the network or cache.
   // Can be used to modify the request or return a different request, for example.
+  /**
+   * Called whenever a request is about to go to the network. Useful when you need 
+   * to change the Request just before it goes to the network.
+   */
   requestWillFetch?: (options: {
     request: Request;
     event?: ExtendableEvent;
   }) => Promise<Request>;
 
-  // Called when a request fails to be fetched and stored in the cache.
-  // Can be used to register a background sync task, for example.
-  fetchDidFail?: (options: {
-    request: Request;
-    error: Error;
-    event?: ExtendableEvent;
-  }) => Promise<void | null | undefined>;
-
   // Called before a response is stored in the cache.
+  /**
+   * Called before a Response is used to update a cache. In this method, 
+   * the response can be changed before it's added to the cache, or you can 
+   * return null to avoid updating the cache entirely.
+   */
   cacheWillUpdate?: (options: {
-    request: Request;
     response: Response;
+    request: Request;
     event?: ExtendableEvent;
-  }) => Promise<Response | void | null | undefined>;
+  }) => Promise<Response | null>;
 
   // Called after a response is stored in the cache.
-  // Can be used to perform additional actions, such as analytics tracking.
+  /**
+   * Called when a new entry is added to a cache or if an existing entry is updated. 
+   * Plugins that use this method may be useful when you want to perform an action after a cache update.
+   */
   cacheDidUpdate?: (options: {
     cacheName: string;
     request: Request;
@@ -37,80 +41,38 @@ export declare interface StrategyPlugin {
   }) => Promise<void>;
 
   // Called before a cached response is used to respond to a fetch event.
+  /**
+   * This is called just before a response from a cache is used, which allows you to examine that 
+   * response. At this point in time, you could either return a different response, or return null (fetch from server at all costs?).
+   */
   cachedResponseWillBeUsed?: (options: {
     cacheName: string;
     request: Request;
     matchOptions: CacheQueryMatchOptions;
     cachedResponse: Response;
     event?: ExtendableEvent;
-  }) => Promise<Response | null | void | undefined>;
-
-  // Called when a cached response is deleted from the cache, either explicitly or due to cache eviction.
-  // Can be used to perform additional cleanup, such as deleting related data in other caches.
-  cacheDidDelete?: (options: {
-    cacheName: string;
-    request?: Request;
-    cachedResponse?: Response;
-    matchOptions?: CacheQueryMatchOptions;
-  }) => Promise<void>;
-
-  // Called right before a fetch request is made, whether it's made directly by the application or by the service worker.
-  // Can be used to modify the request, for example.
-  fetchDidStart?: (options: { request: Request }) => Promise<void>;
-
-  // Called after a response is received from the network, but before it's stored in the cache.
-  // Can be used to modify the response, for example.
-  fetchDidReceive?: (options: {
-    request: Request;
-    response: Response;
-  }) => Promise<Response>;
+  }) => Promise<Response | null>;
 
   // Called after a fetch request is made and a response is received from the network, but before it's returned to the application.
   // Can be used to modify the response, for example.
+  /**
+   * Called whenever a network request succeeds, regardless of the HTTP response code.
+   */
   fetchDidSucceed?: (options: {
     request: Request;
     response: Response;
     event?: ExtendableEvent;
   }) => Promise<Response>;
 
-  // Called before a cached response is used to respond to a fetch event, to check if it's still valid.
-  // Can be used to implement custom caching logic, for example based on the age or content of the cached response.
-  cachedResponseIsValid?: (options: {
-    cacheName: string;
+  // Called when a request fails to be fetched and stored in the cache.
+  // Can be used to register a background sync task, for example.
+  /**
+   * Called when a network request fails, most likely due to an absence of network connectivity, 
+   * and will not fire when the browser has a network connection, but receives an error (for example, 404 Not Found).
+   */
+  fetchDidFail?: (options: {
     request: Request;
-    matchOptions: CacheQueryMatchOptions;
-    cachedResponse: Response;
-  }) => Promise<boolean>;
-
-  // Called before a response is stored in the cache, to check if it should be cached.
-  // Can be used to implement custom caching logic, for example based on the response headers or status.
-  responseShouldBeCached?: (options: {
-    request: Request;
-    response: Response;
-  }) => Promise<boolean>;
-
-  // Called when a fetch request is made to a URL that is not in the cache, to allow the plugin to fetch it and store it.
-  // Can be used to implement custom caching logic, for example to cache requests that match a certain pattern.
-  cacheableFetchDidFetch?: (options: { request: Request }) => Promise<Response>;
-
-  // Called to check if a cached response has expired or is still valid.
-  // Can be used to prune caches in strategies that are heavily reliant on caches
-  cacheHasExpired?: (options: {
-    cacheName: string;
-    matchOptions: CacheQueryMatchOptions;
-    cachedResponse: Response;
-    request: Request;
+    error: Error;
     event?: ExtendableEvent;
-  }) => Promise<boolean> | boolean;
-
-  // Called on a cache store when a response is about to be deleted. Goes through the cache and removes expired entries.
-  // Can be used to implement custom caching logic, for example to delete responses that match a certain pattern (time-based, for example)
-  cacheWillExpire?: (options: { cache: Cache }) => Promise<void> | void;
-
-  // Go over a particular cache and clear out all the expired caches/responses
-  // Shouldn't be called often. Maybe once a day? Something like sync would work with
-  // this.
-  cachesWillExpire?: (options: {
-    cacheNames: string[];
-  }) => Promise<void> | void;
+  }) => Promise<void | null | undefined>;
 }
